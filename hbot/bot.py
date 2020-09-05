7794bd2f57def807ac686b8c96c903223c476154
Eugeny Bobylev <ebobylev@gmail.com>
2020-09-05 14:13:53 +0300

    added bot_stack.py and modified hbot.py (use bot token)

--- hbot/bot.py
+++ hbot/bot.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 
 import telebot
@@ -6,8 +7,7 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from hbot.bot_stack import BotStack
 
-
-bot = telebot.TeleBot('1395562593:AAFwTa_4KJ1wz6-EfLN-2hlMfmMCrH70whA')
+bot = None
 stack = BotStack()
 data = {}
 
@@ -22,7 +22,7 @@ def create_logger() -> logging.Logger:
     _logger.addHandler(ch)
 
     # create file log handler
-    log_fn = Path('.') / "snapchat_bot.log"
+    log_fn = Path('.') / "hbot.log"
     fh = logging.FileHandler(log_fn, mode='w', encoding='utf-8', delay=False)
     setup_log_handler(fh)
     _logger.addHandler(fh)
@@ -40,14 +40,24 @@ def log_info(msg: str):
         logger.info(msg)
 
 
-if __name__ == '__main__':
-    get_tasks_dict = {
-        'show_cameras': show_cameras,
-        'show_groups': show_groups,
-        'get_home': get_home,
-        'send_email': send_email,
-        'show_calendar': show_calendar
-    }
-    logger = create_logger()
+def log_error(msg: str):
+    if logger:
+        logger.error(msg)
 
-    bot.polling()
+
+if __name__ == '__main__':
+    # get_tasks_dict = {
+    #    'show_cameras': show_cameras,
+    #    'show_groups': show_groups,
+    #    'get_home': get_home,
+    #    'send_email': send_email,
+    #    'show_calendar': show_calendar
+    # }
+    logger = create_logger()
+    token = os.environ.get('HBOT_TOKEN')
+    if token:
+        log_info('bot started')
+        bot = telebot.TeleBot(token)
+        bot.polling()
+    else:
+        log_error("bot token not found")
